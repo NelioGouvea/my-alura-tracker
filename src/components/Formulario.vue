0c95f3fbe07ca84f82111138b06c7ca83e6b84c0
Nélio <nelioalvesgouvea@gmail.com>
2023-04-25 18:40:47 -0300

    Finaliza capítulo 3(Composition API)

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,82 +1,87 @@
 <template>
-  <div class="box formulario">
-    <div class="columns">
-      <div class="column is-5" role="form" aria-label="Formulário para criação de uma nova tarefa">
-        <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar ?" v-model="description"/>
-      </div>
-      <div class="column ">
-        <div class="select">
-          <select v-model="idProjeto">
-            <option value="">Selecione o projeto</option>
-            <option
-                :value="projeto.id"
-                v-for="projeto in projetos"
-                :key="projeto.id"
-            >
-              {{ projeto.nome }}
-            </option>
-          </select>
+    <div class="box formulario">
+        <div class="columns">
+            <div class="column is-5" role="form" aria-label="Formulário para criação de uma nova tarefa">
+                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar ?" v-model="description"/>
+            </div>
+            <div class="column ">
+                <div class="select">
+                    <select v-model="idProjeto">
+                        <option value="">Selecione o projeto</option>
+                        <option
+                                :value="projeto.id"
+                                v-for="projeto in projetos"
+                                :key="projeto.id"
+                        >
+                            {{ projeto.nome }}
+                        </option>
+                    </select>
+                </div>
+            </div>
+            <div class="column is-3">
+                <Temporizador
+                        @timer-ended="timerEnded"
+                />
+            </div>
         </div>
-      </div>
-      <div class="column is-3">
-        <Temporizador
-          @timer-ended="timerEnded"
-        />
-      </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts">
-import {computed, defineComponent} from "vue";
+import {computed, defineComponent, ref} from "vue";
 import Temporizador from "@/components/Temporizador.vue";
 import {useStore} from "vuex";
 import {key} from "@/store";
 
 export default defineComponent({
-  name: 'Formulario',
+    name: 'Formulario',
 
-  emits: ['aoSalvarTarefa'],
+    emits: ['aoSalvarTarefa'],
 
-  components: {
-    Temporizador,
-  },
-
-  data() {
-    return {
-      description: '',
-      idProjeto: '',
-    };
-  },
-
-  methods: {
-    timerEnded(timeSeconds : number ) :void {
-      this.$emit('aoSalvarTarefa', {
-        duracaoEmSegundos: timeSeconds,
-        descricao: this.description,
-        projeto: this.projetos.find(proj => proj.id == this.idProjeto)
-      });
-
-      this.description = '';
+    components: {
+        Temporizador,
     },
-  },
 
-  setup() {
-    const store = useStore(key)
-    return {
-      projetos: computed(() => store.state.projeto.projetos)
+    methods: {
+
+    },
+
+    setup(props, { emit }) {
+        const store = useStore(key)
+
+        const description = ref("")
+        const idProjeto = ref("")
+
+        const projetos = computed(() => store.state.projeto.projetos)
+
+        const timerEnded = (timeSeconds: number): void => {
+            emit('aoSalvarTarefa', {
+                duracaoEmSegundos: timeSeconds,
+                descricao: description.value,
+                projeto: projetos.value.find(proj => proj.id == idProjeto.value)
+            });
+
+            description.value = '';
+        }
+
+        return {
+            projetos,
+            description,
+            idProjeto,
+            timerEnded,
+        }
     }
-  }
 });
 
 </script>
 
 <style scoped>
 .button {
-  margin-left: 8px;
+    margin-left: 8px;
 }
+
 .box {
-  background-color: var(--bg-primario);
-  color: var(--texto-primario);
+    background-color: var(--bg-primario);
+    color: var(--texto-primario);
 }
 </style>
--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -4,10 +4,10 @@
             <div class="field">
                 <label for="nomeDoProjeto" class="label"> Nome do Projeto </label>
                 <input
-                    type="text"
-                    class="input"
-                    v-model="nomeDoProjeto"
-                    id="nomeDoProjeto"
+                        type="text"
+                        class="input"
+                        v-model="nomeDoProjeto"
+                        id="nomeDoProjeto"
                 />
             </div>
             <div class="field">
@@ -18,11 +18,12 @@
 </template>
 
 <script lang="ts">
-import {defineComponent} from "vue";
+import {defineComponent, ref} from "vue";
 import {useStore} from "@/store";
 import {TipoNotificacao} from "@/interfaces/INotificacao";
 import useNotificador from '@/hooks/notificador'
 import {ALTERAR_PROJETO, CADASTRAR_PROJETO} from "@/store/tipo-acoes";
+import {useRouter} from "vue-router";
 
 export default defineComponent({
     name: 'Formulario',
@@ -33,44 +34,39 @@ export default defineComponent({
         }
     },
 
-    mounted() {
-        if (this.id) {
-            const projeto = this.store.state.projeto.projetos.find(proj => proj.id == this.id)
-            this.nomeDoProjeto = projeto?.nome || ''
-        }
-    },
-
-    data() {
-        return {
-            nomeDoProjeto: '',
-        };
-    },
-
-    methods: {
-        salvar() {
-            if(this.id){
-                this.store.dispatch(ALTERAR_PROJETO, {
-                    id: this.id,
-                    nome: this.nomeDoProjeto
-                }).then(() => this.showSuccessMessage())
-            }else {
-                this.store.dispatch(CADASTRAR_PROJETO, this.nomeDoProjeto)
-                    .then(() => this.showSuccessMessage())
-            }
-        },
-        showSuccessMessage() {
-            this.nomeDoProjeto = '';
-            this.notificar(TipoNotificacao.SUCESSO, 'Excelente!', 'O projeto foi acadastrado com sucesso!')
-            this.$router.push('/projetos')
-        }
-    },
-
-    setup() {
+    setup(props) {
+        const router = useRouter()
         const store = useStore()
-        const { notificar } = useNotificador()
+        const {notificar} = useNotificador()
+
+        const nomeDoProjeto = ref("")
+        if (props.id) {
+            const projeto = store.state.projeto.projetos.find(proj => proj.id == props.id)
+            nomeDoProjeto.value = projeto?.nome || ''
+        }
+
+        const salvar = () => {
+                if (props.id) {
+                    store.dispatch(ALTERAR_PROJETO, {
+                        id: props.id,
+                        nome: nomeDoProjeto.value
+                    }).then(() => showSuccessMessage())
+                } else {
+                    store.dispatch(CADASTRAR_PROJETO, nomeDoProjeto.value)
+                        .then(() => showSuccessMessage())
+                }
+        }
+
+        const showSuccessMessage = () => {
+            nomeDoProjeto.value = '';
+            notificar(TipoNotificacao.SUCESSO, 'Excelente!', 'O projeto foi cadastrado com sucesso!')
+            router.push('/projetos')
+        }
+
         return {
-            store,
-            notificar
+            nomeDoProjeto,
+            salvar,
+            showSuccessMessage,
         }
     },
 })
